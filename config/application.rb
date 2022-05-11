a4acf74a7e85e92c6e39d6539ec29e920ef44bcc
maekawanaoki <personalpeach10@icloud.com>
2022-05-11 12:11:05 +0900

    add: usersテーブルにusernameカラムの追加
    内容:　ユーザーネームを日記、レビュー、プロフィールに記載するので、中心になるusersモデルから引用するため
    
    fix: configの記載場所間違いの修正
    
    内容：I18nのコードの記載箇所に誤りがあった為、修正

--- config/application.rb
+++ config/application.rb
@@ -5,12 +5,12 @@ require "rails/all"
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
-config.i18n.default_locale = :ja
 
 module EscapegameReviews
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
+    config.i18n.default_locale = :ja
 
     # Configuration for the application, engines, and railties goes here.
     #
